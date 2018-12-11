174755352af148037d9046ab9532ee302e4fc9a7
Mayguen Ojeda <mayguen.ojeda88@gmail.com>
2018-12-11 02:09:51 -0500

    Updates

---
+++ precache-manifest.7615bf19bfe25a16123d0397ae19dcf8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b55b8faa1795e3878ba0",
+    "url": "/hang-man/static/js/runtime~main.b55b8faa.js"
+  },
+  {
+    "revision": "c07651d93d3ad28e7e9b",
+    "url": "/hang-man/static/js/main.c07651d9.chunk.js"
+  },
+  {
+    "revision": "4901f13b0f2128ed85e0",
+    "url": "/hang-man/static/js/1.4901f13b.chunk.js"
+  },
+  {
+    "revision": "c07651d93d3ad28e7e9b",
+    "url": "/hang-man/static/css/main.3b385830.chunk.css"
+  },
+  {
+    "revision": "b38fc1995bd1f4392505a080027d3515",
+    "url": "/hang-man/index.html"
+  }
+];
